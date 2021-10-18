3b7553745c2afb48af0889d6a272da46b512a81d
Your Name <you@example.com>
2021-10-18 11:17:16 -0300

    scroll

--- MyApplication/app/src/main/java/com/example/myapplication/NovoUsuarioActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/NovoUsuarioActivity.kt
@@ -1,11 +1,77 @@
 package com.example.myapplication
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
 
 class NovoUsuarioActivity : AppCompatActivity() {
+
+    lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_novo_usuario)
+
+        editEmail = findViewById(R.id.edit_email)
+        editSenha = findViewById(R.id.edit_senha)
+
+        supportActionBar!!.title = "Novo usuário"
+
+
+        //Criar uma calendário
+        val calendario = Calendar.getInstance()
+
+        //Determinar os dados(dia, mês e ano) do calendário
+        val ano = calendario.get(Calendar.YEAR)
+        val mes = calendario.get(Calendar.MONTH)
+        val dia = calendario.get(Calendar.DAY_OF_MONTH)
+
+        //Abrir o componente DatePicker
+        val etDataNascimento = findViewById<EditText>(R.id.et_data)
+
+        etDataNascimento.setOnClickListener {
+            val dp = DatePickerDialog(this,
+                    DatePickerDialog.OnDateSetListener { _, _ano, _mes, _dia ->
+                        etDataNascimento.setText("$_dia/${_mes + 1}/$_ano")
+                    }, ano, mes, dia)
+
+            dp.show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (validarCampos()){
+
+        }
+
+        return true
+    }
+
+    fun validarCampos(): Boolean {
+        var valido = true
+        if (editEmail.text.isEmpty()) {
+            editEmail.error = "O e-mail é obrigatório!"
+            valido = false
+        }
+        if (editSenha.text.isEmpty()) {
+            editSenha.error = "A senha é obrigatória!"
+            valido = false
+        }
+        return false
     }
 }
